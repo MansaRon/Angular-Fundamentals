ca826d097d01ab67a4ade120ab4ecd06e2d5554c
Thendo Ramashia <kramashia101@gmail.com>
2024-08-27 20:36:58 +0200

    Adding route for single product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'product-details', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule) }, { path: 'product-shop', loadChildren: () => import('./product-shop/product-shop.module').then(m => m.ProductShopModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: '/product-details', pathMatch: 'full' },
+  // Wildcard route for a 404 page
+  // { path: '**', component: PageNotFoundComponent },
+  { path: 'product-details', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule) }, 
+  { path: 'product-shop/:productId', loadChildren: () => import('./product-shop/product-shop.module').then(m => m.ProductShopModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
