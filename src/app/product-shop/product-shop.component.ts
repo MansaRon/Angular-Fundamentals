1e4147202dc5076a97543195d5aa3a40c64fab81
Thendo Ramashia <kramashia101@gmail.com>
2024-08-28 20:59:13 +0200

    Product shop details

--- src/app/product-shop/product-shop.component.ts
+++ src/app/product-shop/product-shop.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Products } from '../data/product';
+import { Product } from '../data/productInterface';
 
 @Component({
   selector: 'app-product-shop',
   templateUrl: './product-shop.component.html',
   styleUrls: ['./product-shop.component.css']
 })
-export class ProductShopComponent {
+export class ProductShopComponent implements OnInit  {
+  product?: Product;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get("productId"));
+    this.product = Products.find(findProduct => findProduct.id === id);
+  }
 
 }
