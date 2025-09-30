04c08a960b89b70885790f425f8f2c33a1dfabde
Thendo <kramashia101@gmail.com>
2025-09-30 21:46:31 +0200

    feat: fix user login on navbar, add logic for thank you component

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly STORAGE_KEY = 'isLogged';
+  private readonly _isLogged$ = new BehaviorSubject<boolean>(
+    localStorage.getItem('isLogged') === 'true',
+  );
+  readonly isLogged$ = this._isLogged$.asObservable();
 
-  constructor(private router: Router) { }
-  
+  constructor(private router: Router) {}
+
   login() {
-    this.router.navigate(['/']);
-    localStorage.setItem('isLogged', 'true');
+    localStorage.setItem(this.STORAGE_KEY, 'true');
+    this._isLogged$.next(true);
   }
 
   logout() {
+    localStorage.removeItem(this.STORAGE_KEY);
+    this._isLogged$.next(false);
     this.router.navigate(['/']);
-    localStorage.removeItem('isLogged');
   }
 
   isLoggedIn() {
-    return localStorage.getItem('isLogged');
+    return this._isLogged$.value;
   }
 }
--- src/app/thank-you/thank-you.component.ts
+++ src/app/thank-you/thank-you.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartStore } from '../store/cart.store';
 
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
-  styleUrls: ['./thank-you.component.css']
+  styleUrls: ['./thank-you.component.css'],
 })
 export class ThankYouComponent {
+  constructor(
+    private cartStore: CartStore,
+    private router: Router,
+  ) {}
 
+  onReturnShopping() {
+    this.cartStore.setProductsInCart([]);
+    this.router.navigate(['/']);
+  }
 }
